382e2202044532131a518853dbc11154390d2ae1
Hero-creator0 <mogalemokgosi389@gmail.com>
2026-04-08 11:06:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+body{
+    font-family: Arial;
+    text-align: center;
+    background-color: #f4f4f4;
+}
+
+h1{
+    color: #333;
+}
+
+input{
+    padding:10px;
+    width:200px;
+}
+
+button{
+    padding:10px;
+    background:blue;
+    color:white;
+    border:none;
+}
+
+li{
+    margin:10px;
+    list-style:none;
+}
