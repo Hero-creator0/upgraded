74dc2aad81342c5125fcf0136cdb296409dfeff3
Hero-creator0 <mogalemokgosi389@gmail.com>
2026-04-14 22:23:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ li{
     margin:10px;
     list-style:none;
 }
+function addTask(){
+    let input = document.getElementById("taskInput").value;
+
+    if(input === "") return;
+
+    let list = document.getElementById("taskList");
+
+    let li = document.createElement("li");
+    li.textContent = input;
+
+    list.appendChild(li);
+
+    document.getElementById("taskInput").value = "";
+}
